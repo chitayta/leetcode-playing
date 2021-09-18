7e3c67956408c4246ecdeaa40801d52b6252d8ed
tay.tc6 <tay.tc6@gmail.com>
2021-09-19 00:12:40 +0700

    feat: init repository

---
+++ src/main/java/xyz/taycta/leetcode/playing/LeetCodePlayingApplication.java
@@ -0,0 +1,13 @@
+package xyz.taycta.leetcode.playing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodePlayingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodePlayingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/taycta/leetcode/playing/LeetCodePlayingApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.taycta.leetcode.playing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodePlayingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
